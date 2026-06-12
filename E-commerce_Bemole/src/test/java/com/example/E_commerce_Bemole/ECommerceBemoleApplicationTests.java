190b03b2c95a1742c2e9baa6ae0934ada2023faf
brandiux <brandon.valenzuela247077@potros.itson.edu.mx>
2026-06-12 05:25:07 -0700

    BaseProyecto
    Base del proyecto, clases principales y enums necesarios

---
+++ E-commerce_Bemole/src/test/java/com/example/E_commerce_Bemole/ECommerceBemoleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.E_commerce_Bemole;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommerceBemoleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
