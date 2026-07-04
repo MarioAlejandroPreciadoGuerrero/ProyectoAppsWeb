ff3acf5b9d0b35509b70a7ad2df1e710a4e79bda
adrian252699 <jesus.luzanilla252699@potros.itson.edu.mx>
2026-07-03 17:48:38 -0700

    Proyecto Admin

---
+++ Dashboard-Admin/Bemole_Dashboard_Admin/src/main/java/com/example/Bemole_Dashboard_Admin/BemoleDashboardAdminApplication.java
@@ -0,0 +1,13 @@
+package com.example.Bemole_Dashboard_Admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BemoleDashboardAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BemoleDashboardAdminApplication.class, args);
+	}
+
+}
---
+++ Dashboard-Admin/Bemole_Dashboard_Admin/src/test/java/com/example/Bemole_Dashboard_Admin/BemoleDashboardAdminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Bemole_Dashboard_Admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BemoleDashboardAdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
